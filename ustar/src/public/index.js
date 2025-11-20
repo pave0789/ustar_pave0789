f3f3249cdd657bad188c4e47e3caec4157a1e9a4
pave0789 <leesang8977@gmail.com>
2025-11-20 15:52:11 +0900

    Add files via upload

---
+++ ustar/src/public/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>U-STAR 앱</title>
+    <!-- Tailwind CSS (디자인 도구) 불러오기 -->
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
